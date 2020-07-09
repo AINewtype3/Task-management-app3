017ad4dbb77138b413b010fdd4790d2f01d36ba1
EC2 Default User <ec2-user@ip-172-31-41-76.ap-northeast-1.compute.internal>
2020-07-09 11:04:02 +0000

    finish D7

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,3 +15,13 @@ User.create!( name: "管理者",
                password: password,
                password_confirmation: password)
 end
+
+admin_user = User.first
+guest_user = User.find(2)
+
+50.times do |n|
+  task_name = "タスク#{n + 1}"
+  description = "タスク詳細#{n + 1}"
+  admin_user.tasks.create!(name: task_name, description: description)
+  guest_user.tasks.create!(name: task_name, description: description)
+end
