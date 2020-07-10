a518d65259c99548e4a70cdff6083428d4d639a5
EC2 Default User <ec2-user@ip-172-31-41-76.ap-northeast-1.compute.internal>
2020-07-10 06:49:53 +0000

    finish E4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_action :set_user, only: %i(show edit update destroy)
   before_action :logged_in_user, only: %i(index, show, edit, update, destroy)
   before_action :correct_user, only: %i(edit update)
+  before_action :admin_or_correct, only: %i(show)
   
   def index
     @users = User.paginate(page: params[:page], per_page: 20)
@@ -74,5 +75,13 @@ class UsersController < ApplicationController
     # 現ログインユーザーであるか
   def correct_user
     redirect_to root_url unless current_user?(@user)
+  end
+  
+  # @userが定義されている上で使用する
+  def admin_or_correct
+    unless current_user?(@user) || current_user.admin?
+      flash[:danger] = "権限がありません。"
+      redirect_to root_url
+    end  
   end
 end
