2a3bbd03dbd9d21c68d2658b26c39630132f3a5c
EC2 Default User <ec2-user@ip-172-31-41-76.ap-northeast-1.compute.internal>
2020-07-11 02:51:38 +0000

    Add pagination_options_label

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,8 +16,12 @@ User.create!( name: "管理者",
                password_confirmation: password)
 end
 
+puts "Users Created"
+
+
 admin_user = User.first
 guest_user = User.find(2)
+
 
 50.times do |n|
   task_name = "タスク#{n + 1}"
@@ -25,3 +29,5 @@ guest_user = User.find(2)
   admin_user.tasks.create!(name: task_name, description: description)
   guest_user.tasks.create!(name: task_name, description: description)
 end
+
+puts "Tasks Created"
